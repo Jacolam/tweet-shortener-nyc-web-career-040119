d85b6e50855afeb52a5a507700ad3752628c3191
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-20 15:20:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,14 +2,14 @@
 
 def word_substituter(tweet)
   dictionary = {
-    hello: hi,
+    hello: "hi",
     to: "2",
     two: "2",
     too: "2",
     :for => "4",
     four: "4",
-    be: b,
-    you: u,
+    be: "b",
+    you: "u",
     at: "@",
     :and => "&" }
     
