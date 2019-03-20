d3bbb620c020b1094cddf65ee837bc736f4a4cca
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-20 15:28:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,22 +2,22 @@
 
 def word_substituter(tweet)
   dictionary = {
-    hello: "hi",
-    to: "2",
-    two: "2",
-    too: "2",
-    :for => "4",
-    four: "4",
-    be: "b",
-    you: "u",
-    at: "@",
-    :and => "&" }
+    "hello" =>  "hi",
+    "to" =>  "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you"=>  "u",
+    "at" => "@",
+    "and" => "&" }
     
   tweet_array = tweet.split()
   
   tweet_array.each_with_index do |word , i|
     dictionary.each do |key , substitute|
-      if word == key 
+      if word.downcase == key 
         tweet_array[i] = substitute
       end 
     end 
